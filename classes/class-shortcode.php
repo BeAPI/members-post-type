854706f26c31c841f760612268ec2d1704985568
herewithme <amaury@beapi.fr>
2013-02-28 12:01:28 +0100

    add todo shortcode

--- classes/class-shortcode.php
+++ classes/class-shortcode.php
@@ -1,5 +1,8 @@
 <?php
 class MPT_Shortcode {
+	/**
+	 * Register X shortcodes : [member-registration], [member-login], [member-forget-password], [member-change-password]
+	 */
 	public function __construct() {
 		
 	}

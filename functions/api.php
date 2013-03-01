6a5541a188de4b5a86dfd1e4f428154f6eca6151
herewithme <amaury@beapi.fr>
2013-03-01 12:40:44 +0100

    add function mail exists

--- functions/api.php
+++ functions/api.php
@@ -7,3 +7,15 @@
 function mpt_is_member_logged_in() {
 	return MPT_User_Utility::is_logged_in();
 }
+
+/**
+ * Test email exists on DB
+ * 
+ * @param  string $email  [description]
+ * @return boolean        [description]
+ */
+function mpt_email_exists( $email ) {
+	$test_user = new MPT_User();
+	$test_user->fill_by('email', $email);
+	return $test_user->exists();
+}

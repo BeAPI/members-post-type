a28ba17cba47fed322d92c94df535d0d3db348dc
herewithme <amaury@beapi.fr>
2013-03-04 12:00:29 +0100

    add method for logout

--- classes/class-main.php
+++ classes/class-main.php
@@ -1,6 +1,19 @@
 <?php
 class MPT_Main {
 	public function __construct() {
+		add_action('init', array(__CLASS__, 'init') );
 	}
 
+	public static function init() {
+		if ( isset($_GET['mpt-action']) && $_GET['mpt-action'] == 'logout' ) {
+			if ( MPT_User_Utility::is_logged_in() ) {
+				MPT_User_Utility::logout();
+				wp_redirect(home_url('/#logout-success') );
+				exit();
+			} else {
+				wp_redirect(home_url('/#logout-error') );
+				exit();
+			}
+		}
+	}
 }

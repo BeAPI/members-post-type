581956bfb9450dfd77186b72452838e373d1c563
herewithme <amaury@beapi.fr>
2013-03-08 14:54:22 +0100

    virtual tag first version 0.2

--- members-post-type.php
+++ members-post-type.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: Members post type
- Version: 0.1
+ Version: 0.2
  Plugin URI: https://github.com/herewithme/members-post-type
  Description: Manage members on WordPress as post type. Implement: post type, authentification, role, clone from WP.
  Author: Amaury Balmer

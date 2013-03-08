ac4380a70150a4316e1bbf6f8d91ac3cf8b22af8
herewithme <amaury@beapi.fr>
2013-03-08 17:12:11 +0100

    add some test, isset notices

--- classes/class-main.php
+++ classes/class-main.php
@@ -18,9 +18,9 @@ class MPT_Main {
 			}
 
 			// Check if request want redirect to somewehre
-			$redirect_to = !empty( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : $redirect_to;
+			$redirect_to = (isset( $_REQUEST['redirect_to'] ) && !empty( $_REQUEST['redirect_to'] )) ? $_REQUEST['redirect_to'] : $redirect_to;
 
-			wp_safe_redirect( $redirect_to );
+			wp_redirect( $redirect_to );
 			exit( );
 		}
 	}
@@ -37,37 +37,38 @@ class MPT_Main {
 		// Update latest date connection
 		update_post_meta( $user_id, '_last_sign_on_date', current_time( 'mysql' ) );
 	}
-	
+
 	/**
 	 * Build action link for MPT actions
 	 */
 	public static function get_action_permalink( $action = '' ) {
 		// Get page ids from options
-		$page_ids = (array) get_option('mpt-pages');
-		
+		$page_ids = (array) get_option( 'mpt-pages' );
+
 		// URL
 		$return_url = '';
-		
+
 		// Different action possible
 		switch( $action ) {
 			case 'registration' :
 			case 'login' :
 			case 'change-password' :
 			case 'lost-password' :
-				if ( isset($page_ids['page-'.$action]) && absint($page_ids['page-'.$action]) > 0 ) {
-					$return_url = get_permalink($page_ids['page-'.$action]);
+				if ( isset( $page_ids['page-' . $action] ) && absint( $page_ids['page-' . $action] ) > 0 ) {
+					$return_url = get_permalink( $page_ids['page-' . $action] );
 				} else {
 					$return_url = '#no-page-id-for-this-action';
 				}
 				break;
 			case 'logout' :
-				$return_url = home_url('/?mpt-action=logout');
+				$return_url = home_url( '/?mpt-action=logout' );
 				break;
 			default :
 				$return_url = '#no-known-action';
 				break;
 		}
-		
+
 		return apply_filters( 'mpt_action_permalink', $return_url, $action );
 	}
+
 }

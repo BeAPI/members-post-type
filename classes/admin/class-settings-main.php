f14576a489aa18f60c311ab1f5bf8827fd44902f
herewithme <amaury@beapi.fr>
2013-03-08 17:10:16 +0100

    add description

--- classes/admin/class-settings-main.php
+++ classes/admin/class-settings-main.php
@@ -11,7 +11,7 @@ class MPT_Admin_Settings_Main {
 		add_settings_section( self::$id . '-section', __( 'General features', 'mpt' ), array(__CLASS__, 'pages_section_callback'), self::$id );
 
 		// Fields
-		add_settings_field( 'allow-signon-email', __( 'Email sign-on', 'mpt' ), array(__CLASS__, 'checkbox_element_callback'), self::$id, self::$id . '-section', array( 'name' => self::$id, 'id' => 'allow-signon-email', 'label' =>  __( 'Allow email sign-on', 'mpt' ) ) );
+		add_settings_field( 'allow-signon-email', __( 'Email sign-on', 'mpt' ), array(__CLASS__, 'checkbox_element_callback'), self::$id, self::$id . '-section', array( 'name' => self::$id, 'id' => 'allow-signon-email', 'label' =>  __( 'Allow email sign-on ?', 'mpt'), 'description' => __('Do not change this value if you have already members! At the risk of breaking your site!', 'mpt' ) ) );
 
 		// DB options
 		register_setting( self::$id, self::$id, array(__CLASS__, 'validate_input') );
@@ -24,6 +24,9 @@ class MPT_Admin_Settings_Main {
 		$html = '<input type="checkbox" id="'.esc_attr( $args['id'] ).'" name="'.esc_attr( $args['name'] ) . '[' . esc_attr( $args['id'] ) . ']" value="1"' . checked( 1, (int) $options[$args['id']], false ) . '/>';
 		$html .= '&nbsp;';
 		$html .= '<label for="'.esc_attr( $args['id'] ).'">'.$args['label'].'</label>';
+		if ( isset($args['description']) ) {
+			$html .= '<p class="description">'.$args['description'].'</p>';
+		}
 		
 		echo $html;
 	}
